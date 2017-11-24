8653027cd0d11d595fdeece02ce9d4bc07bbbaa2
Rachel Abaniwo <rachel.abaniwo@gmail.com>
2017-11-24 19:30:28 +0100

    install react and react dom, and create app component for home page

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Constant with our paths
 const paths = {
   DIST: path.resolve(__dirname, 'client/dist'),
   JS: path.resolve(__dirname, 'client/src/js'),
+  SRC: path.resolve(__dirname, 'client/src')
 };
 
 // Webpack configuration
@@ -13,4 +15,29 @@ module.exports = {
     path: paths.DIST,
     filename: 'app.moin-moin.js'
   },
+  devServer: {
+    contentBase: paths.SRC
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        },
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  }
 };
