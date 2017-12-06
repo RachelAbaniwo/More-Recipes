63c2b7bfe4fc6ab0e23552e55d32bfacbdd072f5
Rachel Abaniwo <rachel.abaniwo@gmail.com>
2017-12-06 16:16:39 +0100

    changes to config file

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: 'postgres',
-    password: null,
+    password: 'postgres',
     database: 'more-recipes',
     host: '127.0.0.1',
     dialect: 'postgres'
